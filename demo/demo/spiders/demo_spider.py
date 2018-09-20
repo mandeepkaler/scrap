5bef95062aacc703909e86415552cb1e925fb49c
Mandeep Kaler <mkaler999@gmail.com>
2018-09-20 14:05:05 +0200

    added lua scripts and session handeling

--- demo/demo/spiders/demo_spider.py
+++ demo/demo/spiders/demo_spider.py
@@ -1,26 +1,91 @@
 import scrapy
+import parsel
 from scrapy.http import FormRequest
+from scrapy_splash import SplashRequest
+from scrapy_splash import SplashFormRequest
+import requests
+import urllib
 
 
+
+lua_init = """ function main(splash)
+  splash:init_cookies(splash.args.cookies)
+  assert(splash:go{
+    splash.args.url,
+    headers=splash.args.headers,
+    http_method=splash.args.http_method,
+    body=splash.args.body,
+    })
+  assert(splash:wait(0.5))
+
+  local entries = splash:history()
+  local last_response = entries[#entries].response
+  return {
+    url = splash:url(),
+    headers = last_response.headers,
+    http_status = last_response.status,
+    cookies = splash:get_cookies(),
+    html = splash:html(),
+  }
+end
+"""
+script = """
+        function main(splash)
+            splash:init_cookies(splash.args.cookies)
+            assert(splash:go(splash.args.url))
+            splash:set_viewport_full()
+            assert(splash:wait(10))
+            local entries = splash:history()
+
+            assert(splash:runjs("$('a#dgInventario__ctl2_descargarBtn').click()"))
+
+            local last_response = entries[#entries].response
+            return {
+                url = splash:url(),
+                headers = last_response.headers,
+                http_status = last_response.status,
+                cookies = splash:get_cookies(),
+                html = splash:html(),
+            }
+            end
+"""
 class LoginSpider(scrapy.Spider):
     name = 'edifichai_spalsh'
     
     start_urls = ['https://edifichai.vw-finance.es/GestImpUI/Enter.aspx']
-    #start_urls = [iframe_url]
-    base_url = 'https://edifichai.vw-finance.es/'
+    base_url = 'https://edifichai.vw-finance.es'
     txtUser = ''
     txtPassword = ''
     txtDependencia = ''
     ComboTipoDep = ''
     btnAceptar = ''
 
+
+    formdata_d = {
+    'secuencia': 'MI6xOzV8xl8=',
+    '__EVENTTARGET' : 'dgInventario$_ctl2$descargarBtn',
+    '__EVENTARGUMENT': '', 
+    '__VIEWSTATE': '/wEPDwUKLTc5NDQ1OTM3NQ9kFgJmD2QWAgIBD2QWAgIHDzwrAAsBAA8WCB4IRGF0YUtleXMWAB4LXyFJdGVtQ291bnQCAR4JUGFnZUNvdW50AgEeFV8hRGF0YVNvdXJjZUl0ZW1Db3VudAIBZBYCZg9kFgZmDw8WBh4GSGVpZ2h0HB4MVGFibGVTZWN0aW9uCyopU3lzdGVtLldlYi5VSS5XZWJDb250cm9scy5UYWJsZVJvd1NlY3Rpb24AHgRfIVNCAoABZBYGZg8PFgIeBFRleHQFC0RlcGVuZGVuY2lhZGQCAQ8PFgIfBwUWw5psdGltYSBhY3R1YWxpemFjacOzbmRkAgIPDxYCHwcFBiZuYnNwO2RkAgEPDxYEHwQcHwYCgAFkFgZmDw8WAh8HBQUwODUwM2RkAgEPDxYCHwcFCjE5LTA5LTIwMThkZAICD2QWAgIBDw8WAh8HBQlEZXNjYXJnYXJkZAICDw8WBh8EHB8FCysEAh8GAoABZBYGZg8PFgIfBwUGJm5ic3A7ZGQCAQ8PFgIfBwUGJm5ic3A7ZGQCAg8PFgIfBwUGJm5ic3A7ZGRkUpvqAUB4AJx5kmsV5YSuMASlr5Y=',
+    '__VIEWSTATEGENERATOR': '0D382CE1',
+    '__EVENTVALIDATION': '/wEdAATxDjd00GL6KQuEpVAhcOoj3g2FJ7ZaNvfIUtTgRkXtqh5imCNaP+hAjiyMTgSvBl7kwrWzZDYu2EKijpnhebiDpeIo3ygkHMvRmR/UzOUegK1tjvk=',
+    'swctextbox1': ' '
+    }
+
+    headers = {
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    def start_requests(self):
+        for url in self.start_urls:
+            yield SplashRequest(url)
 
     #login, without splash
     def parse(self, response):
         if self.txtPassword == '':
             self.logger.error('first add a pasword')
             return
-        return scrapy.FormRequest.from_response(
+        js = self.getFileContentAsString()
+        yield SplashFormRequest.from_response(
                 response,
                 formdata =
                 {
@@ -30,101 +95,99 @@ class LoginSpider(scrapy.Spider):
                     'ComboTipoDep' : self.ComboTipoDep,
                     'btnAceptar'    :   self.btnAceptar
                 },
-                callback = self.scrap_iframe)
+                callback = self.scrap_iframe,
+                endpoint = 'execute',
+                cache_args = ['lua_source'],
+                session_id = 'dummy',
+                args = 
+                {
+                            'html' : 1,
+                            'iframes' : 1,
+                            'lua_source' : lua_init,
+                            'wait' : 10
+                })
 
-    #process the response and process it through splash
+    #process the response through splash
     def scrap_iframe(self, response):
         if 'Errores' in response.body:
             self.log('login error')
         elif 'ifrMenu' in response.body:
             self.log('successfuly logged in')
+                
+        with open('rawLogin.html', 'wb') as f:
+            f.write("%s" % response.body)
+            self.log('Saved after login file %s' % 'rawLogin.html')
+        for key in response.data:
+            print('key {}'.format(key))
 
+        '''string_to_look_for = 'ifrMenu'
         filename = 'responseAfterLog.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-            self.log('Saved after login file %s' % filename)
+        iframe = response.data['childFrames'][1]['html']
+
+        if string_to_look_for not in iframe:
+            for i in range(len(response.data['childFrames'])):
+                if string_to_look_for in response.data['childFrames'][i]['html']:
+                    iframe = response.data['childFrames'][i]['html']
+                    print('index of iframe obj containing #Menu27: {}'.format(i))
+                    break
         
-        url_if = self.base_url + response.css('div#DIVMenu iframe#ifrMenu::attr(src)').extract_first()
+        with open(filename, 'wb') as f:
+            f.write("%s" % iframe.encode('ascii', 'ignore').decode('ascii'))
+            self.log('Saved after login file %s' % filename)
+
+        selector = parsel.Selector(iframe)
+        '''
+        item = {
+            'ifr_src': self.base_url + response.css('div#DIVMenu iframe#ifrMenu::attr(src)').extract_first()  
+        }
+        print('ifr_src {}'.format(item['ifr_src']))
+        url_if = self.base_url + '/GestImpUI/Cobros_mes/Cobros_Mes.aspx'
         js = self.getFileContentAsString()
-        return scrapy.Request(
-                url = url_if,
-                callback = self.extract_data,
-                meta={
-                    'splash' : {
-                        'url' : url_if,
-                        'endpoint' : 'render.html',
-                        'args' : {
-                            'html' : 1,
-                            'js_source' : js,
-                            'wait' : 5
-                        }
-                    }
-                })
+        yield SplashRequest(
+            url = url_if,
+            callback=self.download,
+            endpoint='execute',
+            cache_args = ['lua_source'],
+            args={
+                'png' : 1,
+                'html' : 1,
+                'js_source' : js,
+                'lua_source' : script,
+                'wait': 5
+                },
+            session_id = 'dummy',
+            cookies = response.data['cookies'],
+            headers = response.data['headers']
+        )
+
+    def download(self, response):
+        yield SplashFormRequest.from_response(
+                    response,
+                    formdata = self.formdata_d,
+                    callback = self.extract_data,
+                    endpoint = 'execute',
+                    session_id = 'dummy',
+                    args = 
+                    {
+                                'html' : 1,
+                                'iframes' : 1,
+                                'wait' : 10,
+                                'lua_source' : script
+                    },
+                    cookies = response.data['cookies'],
+                    headers = response.data['headers']
+                    )
 
     def extract_data(self, response):
-        filename = 'responseSplash.html'
+        filename = 'lala.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
             self.log('Saved file %s' % filename)
         # check login succeed before going on
-        if not 'Cobros del mes' in response.body:
-            self.log("Login failed", level = 40)
-            print(response.css("#idMensaje")[0])
-        else:
-            self.log('login successful')
+        for key in response.data:
+            print('response data key: {}'.format(key))
 
     def getFileContentAsString(self):
         with open('demo/spiders/script.js', 'r') as myfile:
             script = myfile.read().replace('\n', '')
         return script
-
-'''
-class LoginSpider(scrapy.Spider):
-    name = 'edifichai'
-    start_urls = ['https://edifichai.vw-finance.es/GestImpUI/Enter.aspx']
-    iframe_url = 'https://edifichai.vw-finance.es/GestImpUI/MenuMain.aspx'
-    #start_urls = [iframe_url]
-    base_url = 'https://edifichai.vw-finance.es/'
-    txtUser = ''
-    txtPassword = ''
-    txtDependencia = ''
-    ComboTipoDep = ''
-    btnAceptar = ''
-
-
-    def parse(self, response):
-        if self.txtPassword == '':
-            self.logger.error('first add a pasword')
-            return
-        return scrapy.FormRequest.from_response(
-                response,
-                formdata =
-                {
-                    'txtUser' : self.txtUser,
-                    'txtPassword' : self.txtPassword,
-                    'txtDependencia' : self.txtDependencia,
-                    'ComboTipoDep' : self.ComboTipoDep,
-                    'btnAceptar'    :   self.btnAceptar
-                },
-                callback = self.scrap_iframe)  
-
-    def scrap_iframe(self, response):
-        url_if = self.base_url + response.css('div#DIVMenu iframe#ifrMenu::attr(src)').extract_first()
-        return scrapy.Request(
-                url = url_if,
-                callback = self.extract_data)
-
-    def extract_data(self, response):
-        filename = 'responseIframe.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-            self.log('Saved file %s' % filename)
-        # check login succeed before going on
-        if not 'Cobros del mes' in response.body:
-            self.log("Login failed", level = 40)
-            print(response.css("#idMensaje")[0])
-        else:
-            self.log('login successful')
-            url_desc = self.base_url + response.css('div#DIVMenu iframe#ifrMenu::attr(src)').extract_first()
-            return
-'''
