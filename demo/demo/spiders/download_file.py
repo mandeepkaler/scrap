6369f18259a89072392ea16313a99283813b16a2
Mandeep Kaler <mkaler999@gmail.com>
2018-09-25 17:53:17 +0200

    removed unnecessary code

--- demo/demo/spiders/download_file.py
+++ demo/demo/spiders/download_file.py
@@ -37,13 +37,7 @@ script = """
             splash:set_viewport_full()
             assert(splash:wait(10))
             local entries = splash:history()
-
-            assert(splash:runjs("document.getElementById('dgInventario__ctl2_descargarBtn').click()"))
-
             assert(splash:runjs("$('#dgInventario__ctl2_descargarBtn').html('changed from lua')"))
-
-            
-
             local last_response = entries[#entries].response
             return {
                 url = splash:url(),
@@ -59,6 +53,7 @@ class LoginSpider(scrapy.Spider):
     
     start_urls = ['https://edifichai.vw-finance.es/GestImpUI/Enter.aspx']
     base_url = 'https://edifichai.vw-finance.es'
+    download_url = base_url + '/GestImpUI/Cobros_mes/Cobros_Mes.aspx'
     txtUser = ''
     txtPassword = ''
     txtDependencia = ''
@@ -66,7 +61,7 @@ class LoginSpider(scrapy.Spider):
     btnAceptar = ''
 
  
-    formdata_d = {
+    formdata = {
         'secuencia': 'FSLfzE4PnLk=',
         '__EVENTTARGET' : 'dgInventario$_ctl2$descargarBtn',
         '__EVENTARGUMENT': '', 
@@ -86,16 +81,21 @@ class LoginSpider(scrapy.Spider):
             'Origin': 'https://edifichai.vw-finance',
             'Cookie' : ''
     }
+
     def start_requests(self):
         for url in self.start_urls:
             yield SplashRequest(url)
+    #check wether the credentials have been set
+    def credentials(self):
+        if self.txtPassword == '' or self.txtDependencia == '' or self.txtUser == '' or self.ComboTipoDep == '' or self.btnAceptar == '' :
+            return False
+        return True
 
-    #login, without splash
+    #attempt login with SplashFormRequest to process javascript
     def parse(self, response):
-        if self.txtPassword == '':
-            self.logger.error('first add a pasword')
+        if not self.credentials():
+            self.logger.error('add the credentials first!')
             return
-        js = self.getFileContentAsString()
         yield SplashFormRequest.from_response(
                 response,
                 formdata =
@@ -113,56 +113,29 @@ class LoginSpider(scrapy.Spider):
                 args = 
                 {
                             'html' : 1,
-                            'iframes' : 1,
                             'lua_source' : lua_init,
-                            'wait' : 10
+                            'wait' : 5
                 })
 
     #process the response through splash
     def scrap_iframe(self, response):
         if 'Errores' in response.body:
-            self.log('login error')
+            self.log('login failed!')
             return
         elif 'ifrMenu' in response.body:
-            self.log('successfuly logged in')
+            self.log('logged in successfuly!')
                 
-        with open('rawLogin.html', 'wb') as f:
+        with open('after_login.html', 'wb') as f:
             f.write("%s" % response.body)
-            self.log('login response html saved at:  %s' % 'rawLogin.html')
-
-        '''string_to_look_for = 'ifrMenu'
-        filename = 'responseAfterLog.html'
-        iframe = response.data['childFrames'][1]['html']
-
-        if string_to_look_for not in iframe:
-            for i in range(len(response.data['childFrames'])):
-                if string_to_look_for in response.data['childFrames'][i]['html']:
-                    iframe = response.data['childFrames'][i]['html']
-                    print('index of iframe obj containing #Menu27: {}'.format(i))
-                    break
+            self.log('response html after login, saved as :  %s' % 'after_login.html')
         
-        with open(filename, 'wb') as f:
-            f.write("%s" % iframe.encode('ascii', 'ignore').decode('ascii'))
-            self.log('Saved after login file %s' % filename)
-
-        selector = parsel.Selector(iframe)
-        
-        item = {
-            'ifr_src': self.base_url + response.css('div#DIVMenu iframe#ifrMenu::attr(src)').extract_first()  
-        }
-        print('ifr_src {}'.format(item['ifr_src']))
-        '''
-        url_if = self.base_url + '/GestImpUI/Cobros_mes/Cobros_Mes.aspx'
-        js = self.getFileContentAsString()
         yield SplashRequest(
-            url = url_if,
+            url = self.download_url,
             callback=self.download,
             endpoint='execute',
             cache_args = ['lua_source'],
             args={
-                'png' : 1,
                 'html' : 1,
-                'js_source' : js,
                 'lua_source' : script,
                 'wait': 5
                 },
@@ -175,55 +148,18 @@ class LoginSpider(scrapy.Spider):
         filename = 'cobros.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
-
-        #self.headers['Cookie'] = response.cookiejar
-
-        print('cookie {}'.format(response.data['cookies']))
-
+        #extract the cookies that are neccesary to maintain the session
         for cookie in response.data['cookies']:
-            #for i in cookie:
-            #    print('{}:{}'.format(i, cookie[i]))
                 self.headers['Cookie'] +=  str(cookie['name']) + "=" + str(cookie['value']) + ';'
 
-        print('cookies string {}'.format(self.headers['Cookie']))
+        self.log('cookies: {}'.format(self.headers['Cookie']))
 
-        cookies = response.data['cookies']
+        formdata = urllib.urlencode(self.formdata)
+        response = requests.post(self.base_url + '/GestImpUI/Cobros_mes/Cobros_Mes.aspx', formdata, allow_redirects=False, headers=self.headers)
 
-       # for c in response.data['cookies']:
-        #    self.headers['Cookie'][c['name']] = c['value']
+        with open('result.csv', 'wb') as f:
+            f.write(response.content)
 
-        data = urllib.urlencode(self.formdata_d)
-        r = requests.post(self.base_url + '/GestImpUI/Cobros_mes/Cobros_Mes.aspx', data, allow_redirects=False, headers=self.headers)
-        with open('manual.csv', 'wb') as f:
-            f.write(r.content)
-
-        yield SplashFormRequest.from_response(
-                    response,
-                    formid =   'form1',
-                    callback = self.extract_data,
-                    endpoint = 'execute',
-                    session_id = 'dummy',
-                    dont_click = True,
-                    clickdata = {
-                        'id' : 'dgInventario__ctl2_descargarBtn'
-                    },
-                    formdata = {},
-                    args = 
-                    {
-                                'html' : 1,
-                                'iframes' : 1,
-                                'wait' : 10,
-                                'lua_source' : script
-                    },
-                    cookies = response.data['cookies'],
-                    headers = response.data['headers']
-                    )
-
-    def extract_data(self, response):
-        filename = 'lala.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-  
     def getFileContentAsString(self):
         with open('demo/spiders/script.js', 'r') as myfile:
             script = myfile.read().replace('\n', '')
--- demo/spider_launcher.py
+++ demo/spider_launcher.py
@@ -1,3 +1,3 @@
 from scrapy.cmdline import execute
-#execute(['scrapy','runspider', '/home/mkaler/Desktop/xerintel/scrap/demo/demo/spiders/splash_fb_spider.py'])
+#execute(['scrapy','runspider', '/home/mkaler/Desktop/xerintel/scrap/demo/demo/spiders/file.py'])
 execute(['scrapy', 'crawl', 'edifichai_spalsh'])
